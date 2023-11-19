ddf34d27e6ac9b4ed198de438d591602590b9fcd
Samat <xzwqt@mail.ru>
2023-11-19 19:41:02 +0700

    add new design for web and refactoring

--- src/components/NewBanner/NewsBanner.jsx
+++ src/components/NewBanner/NewsBanner.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import styles from "./styles.module.css";
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
 import Image from "../ImageComponent/Image";
-import withSkeleton from "../../helpers/hocs/withSkeleton";
 
 const NewsBanner = ({ item }) => {
   return (
@@ -17,6 +16,4 @@ const NewsBanner = ({ item }) => {
   );
 };
 
-const NewsBannerWithSkeleton = withSkeleton(NewsBanner, "banner", 1);
-
-export default NewsBannerWithSkeleton;
+export default NewsBanner;
